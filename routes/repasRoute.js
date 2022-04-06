91acfad04234e1cce39c6f4132528a9656b8030e
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-04-06 12:17:23 +0000

    ✨ finched crud of repas

---
+++ routes/repasRoute.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const verify = require ('../verifyToken/verifyToken');
+
+const Repas = require("../controllers/RepasController");
+
+
+router
+    .route("/search")
+    .post(Repas.searchForRepas);
+
+router
+    .route("/")
+    .get(Repas.getRepas)
+    .post(Repas.createRepas);
+
+router
+    .route("/:idMeal/order")
+    .post(Repas.orderRepas);
+
+router
+    .route("/count")
+    .get(Repas.getCount);
+
+router
+    .route("/recommended")
+    .get(Repas.getRecommendedRepas);
+
+router 
+    .route("/:id")
+    .put(Repas.updateRepas)
+    .delete(Repas.deleteRepas);
+
+module.exports = router;
