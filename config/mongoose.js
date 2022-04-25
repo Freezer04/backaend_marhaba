f30a0c1554ee2af19c0bffc4d3a9abf1b60ec963
Mohammed Kabli <76818640+mohammedkabli@users.noreply.github.com>
2022-04-25 12:12:22 +0000

    🎉 finched the connection with database

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb://127.0.0.1/marhabaApp",
+  "mongodb://127.0.0.1/marhabaApp",{
+    useNewUrlParser: true, 
+
+    useUnifiedTopology: true
+  },
   () => console.log("database connected successfully"),
   (e) => console.log(e.message)
 );
